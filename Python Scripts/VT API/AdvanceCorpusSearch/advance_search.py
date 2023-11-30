4625ca9f32f5e69626a3937cdce61cfc7319c0a5
repulsivityy <55393991+repulsivityy@users.noreply.github.com>
2023-11-30 14:14:06 +0800

    update

--- Python Scripts/VT API/AdvanceCorpusSearch/advance_search.py
+++ Python Scripts/VT API/AdvanceCorpusSearch/advance_search.py
@@ -4,12 +4,12 @@ import urllib
 import requests
 
 #QUERY = 'entity:file submitter:au fs:2023-11-01+ fs:2023-11-14-'
-QUERY = 'entity:ip country:au'
+#QUERY = 'entity:ip country:au last_modification_date:2023-11-01+ last_modification_date:2023-11-14-'
 LIMIT = '10' #Max is 300 results
-ORDER = 'last_submission_date' #see below for order. Default is last_submission for files and url, and last_modification for domains and IP
+#ORDER = 'last_submission_date' #see below for order. Default is last_submission for files and url, and last_modification for domains and IP
 
 def advanced_search(query):
-  url = f'https://www.virustotal.com/api/v3/intelligence/search?query={urllib.parse.quote(query)}&order={ORDER}&descriptors_only=false'
+  url = f'https://www.virustotal.com/api/v3/intelligence/search?query={urllib.parse.quote(query)}&limit={LIMIT}&descriptors_only=false'
   headers = {'Accept': 'application/json', 'x-apikey': os.environ['VT_APIKEY']}
   res = requests.get(url, headers=headers)
   res.raise_for_status()
