7089b34bf3b2acd125aa7229b628a498fdea9dde
repulsivityy <55393991+repulsivityy@users.noreply.github.com>
2024-05-07 23:04:43 +0800

    Update create_update_collections.py

--- Python_Scripts/VT_API/use_cases/create_update_collections.py
+++ Python_Scripts/VT_API/use_cases/create_update_collections.py
@@ -1,7 +1,9 @@
 ######
 # Python Script to 
-# 1. create a collection
-# 2. add in indicators from an advance search
+# 1. create a collection (done)
+# 2. add in indicators from an advance search (partial)
+# 3. get the top 3 malware families, and top 3 threat categories
+# 4. update results from #3 to a google sheets
 # author: dominicchua@google.com
 # currently WIP
 ######
@@ -11,18 +13,24 @@ import os
 from pprint import pprint
 import urllib
 import requests
+import time
 
 
 # variables
-firstseen = "2024-04-22+"
-lastseen = "2024-04-24-"
+firstseen = "2024-04-15+"
+lastseen = "2024-04-30-"
 #firstseen = input("Enter First Seen Start Date (eg 2023-12-01+):")
 #lastseen = input("Enter First Seen End Date (eg 2023-12-31-):")
-LIMIT = '5'
+LIMIT = '300'
 FILE_DETECT = 'entity:file submitter:au fs:'+ firstseen +' fs:'+ lastseen +' p:1+'
-COLLECTION_NAME = 'test collection via api' #name of collection to be used
+#COLLECTION_NAME = input("Enter Collection Name (eg 'Test Collection'): ") #name of collection to be used
+#COLLECTION_DESCRIPTION = input("Enter Collection Description (eg 'For Trends in past 7 days'): ") #description of collection to be used
+COLLECTION_NAME = "test collection"
+COLLECTION_DESCRIPTION = "test description"
 
+################
 #advance search
+################
 def file(query): 
     url = f'https://www.virustotal.com/api/v3/intelligence/search?query={urllib.parse.quote(query)}&limit={LIMIT}&descriptors_only=false'
     headers = {'Accept': 'application/json', 'x-apikey': os.environ['VT_APIKEY']}
@@ -30,7 +38,9 @@ def file(query):
     res.raise_for_status()
     return res.json()
 
-#create a collection
+################
+#create and update collection
+################
 def create_collection(collection, hashes):
     url = f"https://www.virustotal.com/api/v3/collections"
     headers = {'Accept': 'application/json', "content-type": "application/json", 'x-apikey': os.environ['VT_APIKEY']}
@@ -39,7 +49,7 @@ def create_collection(collection, hashes):
 	    "data": {
 		    "attributes": {
 			    "name": COLLECTION_NAME,
-			    "description": "This is how to create a new collection via API."
+			    "description": COLLECTION_DESCRIPTION
 	    	},
 		    "relationships": {
 			    "files": {
@@ -55,17 +65,58 @@ def create_collection(collection, hashes):
 	    }
     }
     response = requests.post(url, json=payload, headers=headers)
+    print("\nCollection Create Successfully\n")
+    return response.json()
+
+#######################
+#get collection details
+#######################
+def get_collection(id):
+    url = f"https://www.virustotal.com/api/v3/collections/{id}"
+    headers = {'Accept': 'application/json', 'x-apikey': os.environ['VT_APIKEY']}
+    response = requests.get(url, headers=headers) 
     print(response.text)
+    #return res.json()
 
-res = file(FILE_DETECT)
+#######################
+#delete collection
+#######################
+def delete_collection(id):
+    url = f"https://www.virustotal.com/api/v3/collections/{id}"
+    headers = {'Accept': 'application/json', "content-type": "application/json", 'x-apikey': os.environ['VT_APIKEY']}
+    res = requests.delete(url, headers=headers)
+    print(res.text)
 
-hashes = []  # Initialize an empty list to store hashes
 
-if "data" in res:
-    for item in res["data"]:
-        if "attributes" in item and "sha256" in item["attributes"]:
-            hashes.append(item["attributes"]["sha256"])
+#######################
+#main
+#######################
+try:
+    res = file(FILE_DETECT)
+    hashes = []  # Initialize an empty list to store hashes
+    vt_col_link = "https://www.virustotal.com/gui/collection/" # Link to VT collection
 
-pprint("Extracted Hashes:")
-pprint(hashes)
-create_collection(COLLECTION_NAME, hashes)
+    if "data" in res:
+        for item in res["data"]:
+            if "attributes" in item and "sha256" in item["attributes"]:
+                hashes.append(item["attributes"]["sha256"])
+
+    pprint(f"Total Number of Hashes: {len(hashes)}") #Print number of hashes
+    collection_link = create_collection(COLLECTION_NAME, hashes)  # Get JSON response
+    collection_id = collection_link["data"]["id"]  # Extract Collection ID from JSON response
+    print("Collection ID:", collection_id) # Print the Collection ID
+    print("Link to Collection:", vt_col_link + collection_id) # Print link to collection
+    print("\nWaiting for 10 seconds...") #wait needed for commmonalities to be computed
+    time.sleep(10)
+    get_collection(collection_id) # Get collection details
+    #delete_collection(collection_id) # cleanup collection during testing 
+
+############
+#error handling
+############
+except requests.exceptions.RequestException as e:
+    print(f"An error occurred: {e}")
+except requests.exceptions.HTTPError as e:
+    print(f"HTTP error occurred: {e}")
+except Exception as e:
+    print(f"An error occurred: {e}")
