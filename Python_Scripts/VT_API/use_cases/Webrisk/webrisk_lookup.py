00d3131e5c4174c06ef709fb829ea816d12d87be
repulsivityy <55393991+repulsivityy@users.noreply.github.com>
2024-06-19 11:56:24 -0700

    updated

--- Python_Scripts/VT_API/use_cases/Webrisk/webrisk_lookup.py
+++ Python_Scripts/VT_API/use_cases/Webrisk/webrisk_lookup.py
@@ -2,19 +2,17 @@ import requests
 import json
 import os
 import urllib
-import requests
-import pprint
 
 api_key = os.environ['WEBRISK_APIKEY']
 # Change the URL to the URL you are checking
-VT_URL = "http://cashforcars-brisbane.com/cxbshell.php"
+check_url = "http://ratenow.site/"
 
 
 def check_uri(url):
     url = f'https://webrisk.googleapis.com/v1/uris:search?threatTypes=MALWARE&threatTypes=SOCIAL_ENGINEERING&uri={urllib.parse.quote(url)}&key={api_key}'
     response = requests.get(url)
     data = response.json()
-    #print(data)
+    print(data)
     #return data
 
-check_uri(VT_URL)
+check_uri(check_url)
