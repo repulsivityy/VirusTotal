66022355bc1ef99075f78261190198c7a482539a
repulsivityy <55393991+repulsivityy@users.noreply.github.com>
2025-03-15 11:37:55 +0800

    Update advance_search.py

--- Python_Scripts/VT_API/advance_search.py
+++ Python_Scripts/VT_API/advance_search.py
@@ -3,17 +3,15 @@ from pprint import pprint
 import urllib
 import requests
 
-QUERY = 'entity:ip (comment:ransom or comment:c2)'
-LIMIT = '2' #Max is 300 results
+QUERY = 'entity:ip gti_score:60+ p:10+ last_modification_date:1d+'
+LIMIT = '5' #Max is 300 results
 
 def advanced_search(query):
   url = f'https://www.virustotal.com/api/v3/intelligence/search?query={urllib.parse.quote(query)}&limit={LIMIT}&descriptors_only=false'
-  headers = {'Accept': 'application/json', 'x-apikey': os.environ['VT_APIKEY']}
+  headers = {'Accept': 'application/json', 'x-apikey': os.environ['GTI_APIKEY']}
   res = requests.get(url, headers=headers)
   res.raise_for_status()
   return res.json()
 
 res = advanced_search(QUERY)
 pprint(res)
-
-
