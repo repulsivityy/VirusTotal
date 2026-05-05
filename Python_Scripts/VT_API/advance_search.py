57f62e9d5f1fccba83a50e694df1943fb0bd59e8
repulsivityy <55393991+repulsivityy@users.noreply.github.com>
2026-05-05 09:45:01 +0800

    updated

--- Python_Scripts/VT_API/advance_search.py
+++ Python_Scripts/VT_API/advance_search.py
@@ -3,11 +3,11 @@ from pprint import pprint
 import urllib
 import requests
 
-QUERY = 'entity:file type:apk p:10+ have:threat_actor'
-LIMIT = '2' #Max is 300 results per page
+QUERY = 'entity:domain last_modification_date:1d+ p:20+'
+LIMIT = '300' #Max is 300 results per page
 
 def advanced_search(query):
-  url = f'https://www.virustotal.com/api/v3/intelligence/search?query={urllib.parse.quote(query)}&limit={LIMIT}&descriptors_only=false'
+  url = f'https://www.virustotal.com/api/v3/intelligence/search?query={urllib.parse.quote(query)}&limit={LIMIT}&descriptors_only=true'
   headers = {'Accept': 'application/json', 'x-apikey': os.environ['GTI_APIKEY']}
   res = requests.get(url, headers=headers)
   res.raise_for_status()
